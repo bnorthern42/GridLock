7c109bf5c48907f0a667e18168c1070fe68dee68
Brad Northern <bradn4@gmail.com>
2026-06-22 22:25:34 -0500

    test: Configured CI environment to use headless lavapipe Vulkan drivers

--- scripts/run_coverage.sh
+++ scripts/run_coverage.sh
@@ -9,6 +9,6 @@ mkdir -p builddir
 echo "Building and starting Docker test environment..."
 docker compose up --build -d
 
-docker compose exec -T test bash -c "rm -rf builddir/* && meson setup builddir -Db_coverage=true && (meson test -C builddir || true) && mkdir -p builddir/meson-logs/coveragereport && gcovr -r . --html --html-details -o builddir/meson-logs/coveragereport/index.html"
+docker compose exec -T test bash -c "rm -rf builddir/* && meson setup builddir -Db_coverage=true && export VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/lvp_icd.x86_64.json && (meson test -C builddir || true) && mkdir -p builddir/meson-logs/coveragereport && gcovr -r . --html --html-details -o builddir/meson-logs/coveragereport/index.html"
 
 echo "[CI] Coverage report generated at: builddir/meson-logs/coveragereport/index.html"
